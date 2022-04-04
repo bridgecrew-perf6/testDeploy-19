4a724465983de2bc65cc9309c0112a27421b23f1
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-04-04 10:00:50 +0530

    redirect

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 
-app.get("/*", (req, res) => {
-  const file = fs.readFileSync(path.join(__dirname, "/assets/Ch22_nonPBR.fbx"));
-  console.log(file);
+// app.use("/", express.static())
+app.use(express.static("assets"));
+
+app.get("/Ch22_nonPBR.fbx", (req, res) => {
+  // const file = fs.readFileSync(path.join(__dirname, "/assets/Ch22_nonPBR.fbx"));
+  // console.log(file);
+  // res.sendFile(path.join(__dirname, "/assets/Ch22_nonPBR.fbx"));
+  res.redirect("Ch22_nonPBR.fbx");
+});
+
+app.get("/Ch31_nonPBR.fbx", (req, res) => {
+  res.redirect("Ch22_nonPBR.fbx");
 });
 
 app.listen(process.env.PORT || 3000, () => {
