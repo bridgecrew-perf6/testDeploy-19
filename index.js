91a3b5368216e83a2b29d60933f38c0c1d68273f
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-04-11 13:17:44 +0530

    cors option changed

--- index.js
+++ index.js
@@ -10,30 +10,33 @@ require("dotenv").config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("assets"));
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "http://localhost:3000, http://localhost:3001"
+//   );
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
 
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
 
-  // Pass to next layer of middleware
-  next();
-});
-app.use(cors());
+//   // Pass to next layer of middleware
+//   next();
+// });
+app.use(cors({ origin: "*" }));
 app.options("*", cors());
 
 app.get("/Ch22_nonPBR.fbx", (req, res) => {
